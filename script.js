ebf52239a7b16876f895f66be01cbc0253e895ef
Tychebt <masse.ju35@gmail.com>
2021-04-18 23:18:30 +0200

    Exercices js

---
+++ script.js
@@ -0,0 +1,16 @@
+function add7 (number){
+return number+7;
+}
+
+function multiply (x,y){
+    return x*y;
+}
+
+function capitalize (str){
+    return str.indexOf(0).toUpperCase();
+}
+
+function lastLetter (str){
+    let length = str.length;
+    return str.indexOf(length-1)
+}
