b493ff23cd8eef088e40a2b91bbc5784eed276f1
Julien <masse.ju35@gmail.com>
2021-04-19 22:25:14 +0200

    PlayRound OK

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ console.log( number+7)
 
 function multiply (x,y){
     console.log( x*y )
-}
+}*/
 
 function capitalize (str){
     let cap = str.slice(0, 1).toUpperCase()
     let norm = str.slice(1)
     let res = cap + norm
-    console.log( res  )
+    return res
 }
 
 function lastLetter (str){
     let length = str.length;
     console.log(str.slice(length-1))
-} */
+} 
 
-function computerPlay (){
-    let res = getRandomInt(3)
+function computerSelection(){
+    let res = Math.floor(Math.random() * 3)
     if(res === 0){
         return "Rock"
     }
@@ -28,5 +28,45 @@ function computerPlay (){
     }
     else if(res === 2){
         return "Scissors"
+    }
+}
+
+
+function playerSelection(){
+    let res = prompt('Rock, Paper or Scissors ?', 'Rock')
+    res = capitalize(res)
+    return res
+}
+
+function playRound(computerSelection, playerSelection){
+    computerSelection = computerSelection()
+    playerSelection = playerSelection()
+
+    let res
+
+    if(computerSelection == playerSelection){
+        res = `Tie! ${computerSelection} and ${playerSelection}`
+    }
+    else if(computerSelection == "Rock" && playerSelection === "Scissors"){
+        res = `You Lose! ${computerSelection} beat ${playerSelection}`
+    }
+    else if(computerSelection == "Paper" && playerSelection === "Rock"){
+        res = `You Lose! ${computerSelection} beat ${playerSelection}`
+    }
+    else if(computerSelection == "Scissors" && playerSelection === "Paper"){
+        res = `You Lose! ${computerSelection} beat ${playerSelection}`
+    }
+    else {res = `You Win! ${playerSelection} beat ${computerSelection}`}
+    return res
+}
+
+function game(){
+    let countPC = 0
+    let countPlayer = 0
+
+    for(let i = 0; i<5; i++){
+        playRound(computerSelection, playerSelection).indexOf == -1
+
+        
     }
 }
